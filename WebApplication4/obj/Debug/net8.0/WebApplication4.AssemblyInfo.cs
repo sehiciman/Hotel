78d6848111637f7230ea57f43704fb8f4929896a
imansehic <isehic1@etf.unsa.ba>
2025-03-01 12:59:47 +0100

    prikaz lokacija na mapi

--- WebApplication4/obj/Debug/net8.0/WebApplication4.AssemblyInfo.cs
+++ WebApplication4/obj/Debug/net8.0/WebApplication4.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a271f71a029554201547cc004fd6a51df3638fd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd0448e411a4575eae38a66e773f879b42d69e73")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

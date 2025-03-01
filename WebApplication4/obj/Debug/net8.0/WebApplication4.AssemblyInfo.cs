6180fca01d69178cadb56f0c066af19ef0b2ec39
imansehic <isehic1@etf.unsa.ba>
2025-03-01 13:02:50 +0100

    finalni 1

--- WebApplication4/obj/Debug/net8.0/WebApplication4.AssemblyInfo.cs
+++ WebApplication4/obj/Debug/net8.0/WebApplication4.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd0448e411a4575eae38a66e773f879b42d69e73")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78d6848111637f7230ea57f43704fb8f4929896a")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
